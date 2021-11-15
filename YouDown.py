21d97630942706309793e883b4c8fbc9a46fb951
techguytechtips <vance_peever@hotmail.com>
2021-11-15 13:37:04 -0500

    why am I even still updating this.

--- YouDown.py
+++ YouDown.py
@@ -2,31 +2,12 @@ from __future__ import unicode_literals
 import youtube_dl
 import shutil
 import os
+import threading
 from tkinter import *
-
-class Window(Frame):                   
-    def __init__(self, master = None):
-        Frame.__init__(self, master)
-        self.master = master		
-        self.init_window()
-    def init_window(self):
-        self.master.title("YouDown")
-        self.pack(fill=BOTH, expand=1)
-        runButton = Button(self, text="Mp3", command=self.mp3, fg='#282828', bg='#FF0000')
-        runButton.place(x=145, y=210)
-        runButton = Button(self, text="Mp4", command=self.mp4, fg='#282828', bg='#FF0000')
-        runButton.place(x=205, y=210)
-
-    def mp3(self):
-        try:
-            os.mkdir("Videos")
-        except:
-            print("Folder 'Videos' Already Exists")
-        if os.name == "nt":
-            shutil.copy("ffmpeg.exe","Videos/ffmpeg.exe")
-        os.chdir("Videos")
-        vidlist = list = video.get().split()
-        ydl_opts = {
+dlthread = 0
+def download(Format, vidlist):
+        if Format == "mp3":
+             ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
@@ -39,29 +20,54 @@ class Window(Frame):
             'prefer_ffmpeg': True,
             'keepvideo': False
             }
+        else:
+            ydl_opts = {}
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                try:
-                    ydl.download(vidlist)
-                except Exception as e:
-                    print("Failed to download video! Error:")
-                    print(e)
+            try:
+                print(vidlist)
+                ydl.download(vidlist)
+            except Exception as e:
+                print("Failed to download video! Error:")
+                print(e)
         print("Done")
-    def mp4(self):
+
+class Window(Frame):
+
+    def __init__(self, master = None):
+        Frame.__init__(self, master)
+        self.master = master		
+        self.init_window()
+    def init_window(self):
+        self.master.title("YouDown")
+        self.pack(fill=BOTH, expand=1)
+        runButton = Button(self, text="Mp3", command=self.mp3, fg='#282828', bg='#FF0000')
+        runButton.place(x=145, y=210)
+        runButton = Button(self, text="Mp4", command=self.mp4, fg='#282828', bg='#FF0000')
+        runButton.place(x=205, y=210)
+    def mp3(self):
+        global dlthread
         try:
             os.mkdir("Videos")
         except:
             print("Folder 'Videos' Already Exists")
+        if os.name == "nt":
+            shutil.copy("ffmpeg.exe","Videos/ffmpeg.exe")
         os.chdir("Videos")
-        vidlist = list = video.get().split()
-        ydl_opts = {}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                try:
-                    ydl.download(vidlist)
-                except Exception as e:
-                    print("Failed to download video! Error:")
-                    print(e)
+        vidlist = video.get().split()
 
-        print("Done")
+        dlthread = threading.Thread(target=download, args=("mp3", vidlist,))
+        dlthread.start()
+    def mp4(self):
+        global dlthread
+        try:
+            os.mkdir("Videos")
+        except:
+            print("Folder 'Videos' Already Exists")
+
+        os.chdir("Videos")
+        vidlist = video.get().split()
+        dlthread = threading.Thread(target=download, args=("mp4", vidlist,))
+        dlthread.start()
 
 root = Tk()
 
@@ -80,3 +86,4 @@ label1 = Label(root, text="Enter Video(s) or Playlist: ", font=("arial",12, "bol
 entry_box = Entry(root, textvariable=(video), width=50, bg="#FFFFFF").place(x=50, y=60)
 
 root.mainloop()
+dlthread.join()
